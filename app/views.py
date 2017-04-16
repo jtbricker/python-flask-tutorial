64c9333e53f54bf438043105fbee55b33ae64bc1
Justin Bricker <jt.bricker@gmail.com>
2017-04-16 01:49:00 -0400

    Profile Page and Avatars
    * Add avatar method to User model which gets avatar from Gravatar
    * Update base template to include link to profile when logged in
    * Add post template to render posts the same way on all pages that
    require displaying posts
    * Add user template to display user avatar, information and posts
    * Add view endpoint to return user page
    * Add last_seen and about_me fields to the User model
    * Add edit view, template, form
    
    Through https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vi-profile-page-and-avatars

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .models import User
 
 @app.route('/')
@@ -66,7 +67,42 @@ def logout():
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    u = User.query.filter_by(nickname=nickname).first()
+    if u is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': u, 'body': 'Test post #1'},
+        {'author': u, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=u,
+                           posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
